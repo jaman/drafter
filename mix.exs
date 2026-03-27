3c53e6b6bce058a97d14dafdf59dab166a9eacd8
Jarius Jenkins <jarius.jenkins@gmail.com>
2026-03-27 10:16:13 +0900

    Versoun bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Drafter.MixProject do
   def project do
     [
       app: :drafter,
-      version: "0.2.9",
+      version: "0.2.10",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:elixir_make | Mix.compilers()],
