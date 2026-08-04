b5f7ef9b633accf6f810e39cfacf22c709417a77
Jarius Jenkins <jarius.jenkins@gmail.com>
2026-08-04 11:46:11 +0900

    Release v0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Drafter.MixProject do
   def project do
     [
       app: :drafter,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:elixir_make | Mix.compilers()],
